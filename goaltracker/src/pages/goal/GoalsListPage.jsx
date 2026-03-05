2742268e7f246e3e995fbc839778645205682d2b
SETAYESH <setayeshazizi124@gmail.com>
2026-03-05 20:56:57 +0430

    Update GoalsListPage.jsx

--- goaltracker/src/pages/goal/GoalsListPage.jsx
+++ goaltracker/src/pages/goal/GoalsListPage.jsx
@@ -27,3 +27,30 @@ import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
 import TimelineRoundedIcon from "@mui/icons-material/TimelineRounded";
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
+
+
+import GoalsGrid from "../../components/goals/GoalsGrid";
+import ConfirmDialog from "../../components/common/ConfirmDialog";
+import SectionCard from "../../components/common/SectionCard";
+import { useGoals } from "../../context/GoalsContext";
+import { sortAndFilterGoals } from "../../utils/goals";
+import ExportButton from "../../components/common/ExportButton";
+
+function StatCard({ icon, label, value, color, statCardSx }) {
+  return (
+    <Card elevation={0} sx={statCardSx}>
+      <CardContent>
+        <Stack direction="row" alignItems="center" spacing={1}>
+          <Box sx={{ color }}>{icon}</Box>
+          <Typography variant="body2" color="text.secondary">
+            {label}
+          </Typography>
+        </Stack>
+        <Typography variant="h4" fontWeight={800} sx={{ mt: 1 }}>
+          {value}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
+
